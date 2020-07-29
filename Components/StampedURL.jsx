0aa40aaadfa9f5e87c20068f6f7a4d93440fe935
ksinghj <kartarsjabanda@gmail.com>
2020-07-29 22:09:04 +0100

    finished url there

--- Components/StampedURL.jsx
+++ Components/StampedURL.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import { Text, StyleSheet, View } from "react-native"
 
-const StampedURL = ({ title, url }) => {
+const StampedURL = ({ title, url, hours, minutes, seconds }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.url}>{title}:</Text>
-      <Text>{url}</Text>
+      <Text>
+        {url}&t={hours}h{minutes}m{seconds}s
+      </Text>
     </View>
   )
-  // TODO: make copy to clipboard easy
+  // TODO: make copy to clipboard easy, onpress copy to clipboard with toast
 }
 
 const styles = StyleSheet.create({
